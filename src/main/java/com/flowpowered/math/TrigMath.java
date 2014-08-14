9c4e6ed200bcf154fc8074b1e72a4cc8afe4b09f
Luke Spragg <luke@lukespragg.com>
2014-08-14 14:50:12 -0400

    Minor pom.xml and header updates

--- src/main/java/com/flowpowered/math/TrigMath.java
+++ src/main/java/com/flowpowered/math/TrigMath.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Flow Math, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2013 Spout LLC <http://www.spout.org/>
+ * Copyright (c) 2013 Spout LLC <https://spout.org/>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
